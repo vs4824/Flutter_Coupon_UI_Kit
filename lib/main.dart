3051d43e2c5de865c0f23ca5696ea476d0239106
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-05-16 10:45:34 +0530

    Flutter Coupon UI Kit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_coupon_uikit/samples/horizontal_coupon_example1.dart';
+import 'package:flutter_coupon_uikit/samples/horizontal_coupon_example2.dart';
+import 'samples/vertical_coupon_example.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coupon Cards Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Coupon Cards Demo')),
+      body: const Padding(
+        padding: EdgeInsets.all(14),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              HorizontalCouponExample1(),
+              SizedBox(height: 14),
+              VerticalCouponExample(),
+              SizedBox(height: 14),
+              HorizontalCouponExample2(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
